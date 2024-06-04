ca065f59f05bea71c6f27be3064ff5a89b99093f
MRALISAAD <mr.alisaad@outlook.fr>
2024-06-04 16:10:50 -0400

    ajout de snow flake connector dans le projet

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import streamlit as st
+import snowflake.connector
+
+
+snowflake_account = 'your_account_name'
+snowflake_user = 'your_username'
+snowflake_password = 'your_password'
+snowflake_database = 'your_database_name'
+snowflake_schema = 'your_schema_name'
+snowflake_warehouse = 'your_warehouse_name'
+
 
 def main():
     st.write("Bienvenue sur snowflake demo ")
 
 if __name__ == '__main__':
     main()
+
